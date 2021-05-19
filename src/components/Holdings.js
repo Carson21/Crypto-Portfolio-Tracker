d43a79bf7350cf1778dad5c125d58a5473aa7594
Carson Keavney <carsonkeavney@gmail.com>
2021-05-19 18:32:58 -0400

    Fetch data from CoinGecko and show in a Select Menu

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -2,7 +2,7 @@ const Holdings = ({ coins }) => {
   return (
     <div className='p-4'>
       <h2 className='text-2xl text-center font-sans'>Holdings</h2>
-      {coins.length > 0 &&
+      {coins &&
         coins.map((coin, index) => {
           return <p key={index}>{coin.name}</p>
         })}
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,30 +1,40 @@
-import { Listbox } from '@headlessui/react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Select from 'react-select'
 
 const Transactions = ({ coins, addCoin, setCoin }) => {
   const [selectedCoin, setSelectedCoin] = useState()
+  const [geckoCoins, setGeckoCoins] = useState()
+
+  useEffect(() => {
+    fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=500&page=1&sparkline=false'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setGeckoCoins(
+          data.map((coin) => {
+            return { value: coin.symbol, label: capatilize(coin.id) }
+          })
+        )
+      })
+  }, [])
 
   const capatilize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1)
+    let split = str.split('-')
+    split = split.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    return split.join(' ')
+  }
+
+  const handleChoose = (coin) => {
+    setSelectedCoin(coin.value)
   }
 
   return (
     <div className='p-4 w-1/3 border border-gray-500 border-solid'>
       <h2 className='text-2xl text-center font-sans'>Transactions</h2>
-      {coins.length > 0 && (
-        <Listbox value={selectedCoin} onChange={setSelectedCoin}>
-          <Listbox.Button className='block'>Select Coin</Listbox.Button>
-          <Listbox.Options>
-            {coins.map((coin, index) => {
-              return (
-                <Listbox.Option key={index} value={coin}>
-                  {capatilize(coin.name)}
-                </Listbox.Option>
-              )
-            })}
-          </Listbox.Options>
-        </Listbox>
-      )}
+      <Select options={geckoCoins} onChange={handleChoose} />
       <button
         onClick={() => {
           if (!(selectedCoin === undefined)) {
