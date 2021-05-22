38d173108c9270fa52f4976d06ba48d6ff07a0de
Carson Keavney <carsonkeavney@gmail.com>
2021-05-22 18:26:51 -0400

    Added scroll bar for Holdings Section when coins list gets too large

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -2,12 +2,14 @@ import CoinSummary from './CoinSummary'
 
 const Holdings = ({ coins }) => {
   return (
-    <div className='overflow-auto p-4'>
-      <h2 className='text-2xl text-center font-bold mt-4'>Holdings</h2>
-      {coins &&
-        coins.map((coin, index) => {
-          return <CoinSummary key={index} coin={coin} />
-        })}
+    <div className='h-full p-4 flex flex-col justify-evenly'>
+      <h2 className='text-2xl text-center font-bold my-2'>Holdings</h2>
+      <div className='overflow-auto h-3/4'>
+        {coins &&
+          coins.map((coin, index) => {
+            return <CoinSummary key={index} coin={coin} />
+          })}
+      </div>
     </div>
   )
 }
