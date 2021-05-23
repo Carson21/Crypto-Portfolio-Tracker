5608882ea8601601db1f10c934657bc354b3a311
Carson Keavney <carsonkeavney@gmail.com>
2021-05-22 22:50:27 -0400

    Change sizing and spacing of children of Holdings

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -2,9 +2,9 @@ import CoinSummary from './CoinSummary'
 
 const Holdings = ({ coins }) => {
   return (
-    <div className='h-full p-4 flex flex-col justify-evenly'>
-      <h2 className='text-2xl text-center font-bold my-2'>Holdings</h2>
-      <div className='overflow-auto h-3/4'>
+    <div className='h-full p-4 flex flex-col justify-evenly gap-6'>
+      <h2 className='text-2xl text-center font-bold mt-2'>Holdings</h2>
+      <div className='overflow-auto mb-2'>
         {coins &&
           coins.map((coin, index) => {
             return <CoinSummary key={index} coin={coin} />
